94d8560d00c7d5e1f9db96d406a74aa65c8ebf3a
Muhammad Ahmed <muhammadmanzoor16@gmail.com>
2021-02-25 22:16:32 -0600

    Added Student Search Controller

--- app/Http/Controllers/StudentSearchController.php
+++ app/Http/Controllers/StudentSearchController.php
@@ -16,5 +16,72 @@ class StudentSearchController extends Controller
 {
     public function search(Request $request)
     {
+        $students = new student;
+        try {
+            // Construnt Eloquent Query based on query parms. Not using Dynamic SQL to prevent SQL injection
+
+            if ($request->has('first_name')) {
+                $students = Student::where('first_name', $request->first_name)->get();
+            }
+
+            if ($request->has('last_name')) {
+                $students = Student::where('last_name', $request->last_name)->get();
+            }
+
+            if ($request->has('classroom_id')) {
+
+                //Get  all students by classroom only
+                if (!$request->has('last_name') && !$request->has('first_name')) {
+                    $students = Student::where('classroom_id', $request->classroom_id)->get();
+                } else // filter collection for pre-selected students
+                    $students = $students->where('classroom_id', $request->classroom_id);
+            }
+
+            if ($request->has('school_id')) {
+                $sid = $request->school_id;
+
+                /*  SQL Statement - Fet All Students by School Id
+                    select * from students
+                     where classroom_id in ( select classroom_id from classrooms
+                                             where school_id = :school_id)
+                
+                //col reference issues with this approach
+                $students= Student::whereIn(
+                    'classroom_id',
+                    function ($query) use ($sid) {
+                        $query->select('classroom_id')->from('classrooms')->whereColumn('school_id',$sid );
+                    })->get();
+
+                */
+                // Divide and Conquer - query classrooms first 
+                $cids =  Classroom::select('id')->where('school_id', $sid)->get();
+                $students = Student::whereIn('classroom_id', $cids)->get();
+                
+                if ($request->has('first_name')) {
+                    $students = $students->where('first_name', $request->first_name);
+                }
+                if ($request->has('last_name')) {
+                    $students = $students->where('last_name', $request->last_name);
+                }
+                if ($request->has('classroom_id')) {
+                    $students = $students->where('classroom_id', $request->classroom_id);
+                }
+            }
+
+            // Add full name property
+            if (count($students) > 0) {
+                foreach ($students as $student) {
+                    $full_name = $student['first_name'] . ' ' . $student['last_name'];
+                    $student->full_name = $full_name;
+                }
+            }
+            // Return response
+            $result = ['status' => '200 (Ok)', 'message' => 'Students retrieved successfully', 'data' => ''];
+            $result['data'] = $students;
+            return response($result, 200);
+        } catch (QueryException $ex) {
+            $result['message'] = $ex->getMessage();
+            return response($result, 400);
+        }
     }
 }
